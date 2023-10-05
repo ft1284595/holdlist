53166da553f03c3e6f431950318452621b8e9700
zx <ft1284595@gmail.com>
2023-10-05 10:31:44 -0500

    generate pivot table, but the column is a fixed width.

--- zx_pivot_table.py
+++ zx_pivot_table.py
@@ -1,10 +1,26 @@
 import pandas as pd
-import os
+import numpy as np
+
+
 
 def main():
-    pass
+    df = pd.read_excel("./933.xlsx", dtype=str)
+    df.head()
 
+    pt = pd.pivot_table(df, index=["MAWB","Container No.","HAWB"], values=["CBP Status"], aggfunc=["count"], margins=True)
+    
 
+    print(pt)
+    pt.to_excel("../933-pivot.xlsx")
+    """
+    with pd.ExcelWriter("933.xlsx") as writer:
+        pt.to_excel("../933-pivot.xlsx", sheet_name="zx-pivot-table")
+        worksheet = writer.sheets['zx-pivot-table']
+        for i in range(1,3):
+            worksheet.set_column(i, i, 20)
+
+        writer.save()
+    """
 
 
 
